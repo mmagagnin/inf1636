c3fd88c8618795f746a4f0f36f1796ecbb85a04a
mmagagnin <mmagagninr@gmail.com>
2023-03-12 14:03:35 -0300

    exercises

---
+++ exercicios/EX-02-00/EX0200.bat
@@ -0,0 +1,1 @@
+PATH= C:\Program Files\Java\jdk-13.0.2\bin
---
+++ exercicios/EX-02-00/HelloWorldApp.java
@@ -0,0 +1,11 @@
+/**  
+* The HelloWorldApp class implements an application that  
+* simply displays "Hello World!" to the standard output.  
+*/
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+       //Display "Hello World!"
+       System.out.println("Hello World!");
+    }
+}
